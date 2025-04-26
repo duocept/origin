76a76f0d8e4ba4bfd4101866abc3e2f969c29099
duocept <duocept@gmail.com>
2025-04-26 17:59:53 +0300

    Months correction

--- homeworks_02/task_01/Months/Months/Months.cpp
+++ homeworks_02/task_01/Months/Months/Months.cpp
@@ -13,8 +13,7 @@ enum class Months
 	Сентябрь,
 	Октябрь,
 	Ноябрь,
-	Декабрь,
-	Выход = 0
+	Декабрь
 };
 
 int main()
@@ -22,30 +21,35 @@ int main()
     setlocale(LC_ALL, "rus");
 
 	int num{1};
-	while (num != 0) {
-		std::cout << "Введите номер месяца: ";
-		std::cin >> num;
-		Months months = static_cast<Months>(num);
+    while (true) {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> num;
 
-		switch (months)
-		{
-		case Months::Январь: std::cout << "Январь" << std::endl; break;
-		case Months::Февраль: std::cout << "Февраль" << std::endl; break;
-		case Months::Март: std::cout << "Март" << std::endl; break;
-		case Months::Апрель: std::cout << "Апрель" << std::endl; break;
-		case Months::Май: std::cout << "Май" << std::endl; break;
-		case Months::Июнь: std::cout << "Июнь" << std::endl; break;
-		case Months::Июль: std::cout << "Июль" << std::endl; break;
-		case Months::Август: std::cout << "Август" << std::endl; break;
-		case Months::Сентябрь: std::cout << "Сентябрь" << std::endl; break;
-		case Months::Октябрь: std::cout << "Октябрь" << std::endl; break;
-		case Months::Ноябрь: std::cout << "Ноябрь" << std::endl; break;
-		case Months::Декабрь: std::cout << "Декабрь" << std::endl; break;
-		case Months::Выход: std::cout << "До свидания" << std::endl; break;
-		default:
-			std::cout << "Введён неверный номер" << std::endl; break;
-		}
-	}
+        if (num == 0) {
+            std::cout << "До свидания" << std::endl;
+            break;
+        }
+
+        Months months = static_cast<Months>(num);
+
+        switch (months)
+        {
+        case Months::Январь: std::cout << "Январь" << std::endl; break;
+        case Months::Февраль: std::cout << "Февраль" << std::endl; break;
+        case Months::Март: std::cout << "Март" << std::endl; break;
+        case Months::Апрель: std::cout << "Апрель" << std::endl; break;
+        case Months::Май: std::cout << "Май" << std::endl; break;
+        case Months::Июнь: std::cout << "Июнь" << std::endl; break;
+        case Months::Июль: std::cout << "Июль" << std::endl; break;
+        case Months::Август: std::cout << "Август" << std::endl; break;
+        case Months::Сентябрь: std::cout << "Сентябрь" << std::endl; break;
+        case Months::Октябрь: std::cout << "Октябрь" << std::endl; break;
+        case Months::Ноябрь: std::cout << "Ноябрь" << std::endl; break;
+        case Months::Декабрь: std::cout << "Декабрь" << std::endl; break;
+        default:
+            std::cout << "Введён неверный номер" << std::endl; break;
+        }
+    }
 
 	return EXIT_SUCCESS;
 }
