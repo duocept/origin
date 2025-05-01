2eae420a5192a7a7140b13ae05db334b460b59aa
duocept <duocept@gmail.com>
2025-05-01 20:20:16 +0300

    Homeworks 03 added

---
+++ homeworks_03/task_01/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Calculator {
+private:
+    double num1{};
+    double num2{};
+
+public:
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        return false;
+    }
+
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        return false;
+    }
+
+    double add() const {
+        return num1 + num2;
+    }
+
+    double multiply() const {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() const {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() const {
+        return num2 - num1;
+    }
+
+    double divide_1_2() const {
+        return num1 / num2;
+    }
+
+    double divide_2_1() const {
+        return num2 / num1;
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Calculator calc;
+    double a, b;
+
+    while (true) {
+        std::cout << "Введите num1: ";
+        std::cin >> a;
+        std::cout << "Введите num2: ";
+        std::cin >> b;
+
+        if (calc.set_num1(a) && calc.set_num2(b)) {
+            std::cout << "num1 + num2 = " << calc.add() << std::endl;
+            std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+            std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+            std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+            std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+            std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+        }
+        else {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ homeworks_03/task_02/Counter/Counter/Counter.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class Counter {
+private:
+    int value;
+
+public:
+    Counter() : value(1) {}
+    Counter(int initial) : value(initial) {}
+
+    void increment() {
+        value++;
+    }
+
+    void decrement() {
+        value--;
+    }
+
+    int get_value() const {
+        return value;
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Counter counter;
+    std::string input;
+    int initial_value = 1;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> input;
+
+    if (input == "да" || input == "Да" || input == "ДА") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initial_value;
+        counter = Counter(initial_value);
+    }
+
+    while (true) {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> input;
+
+        if (input == "+") {
+            counter.increment();
+        }
+        else if (input == "-") {
+            counter.decrement();
+        }
+        else if (input == "=") {
+            std::cout << counter.get_value() << std::endl;
+        }
+        else if (input == "x" || input == "х") {
+            std::cout << "До свидания!" << std::endl;
+            break;
+        }
+        else {
+            std::cout << "Неизвестная команда!" << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
