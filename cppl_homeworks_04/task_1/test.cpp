c219670552a30909e03ec79f174371e436177518
duocept <duocept@gmail.com>
2025-09-07 21:54:51 +0300

    cppl_homeworks_04 fix

--- cppl_homeworks_04/task_1/test.cpp
+++ cppl_homeworks_04/task_1/test.cpp
@@ -1,15 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
-#include "list.cpp"
+#include "list.h"
 
-TEST_CASE("Empty() и Size() на свежем списке", "[list][empty][size]") {
+TEST_CASE("Свежесозданный список пуст", "[task1][empty][size]") {
     List lst;
     REQUIRE(lst.Empty());
     REQUIRE(lst.Size() == 0UL);
 }
 
-TEST_CASE("Size() корректно растёт при добавлениях", "[list][size]") {
+TEST_CASE("Size растет при вставках", "[task1][size]") {
     List lst;
-
     lst.PushFront(10);
     REQUIRE_FALSE(lst.Empty());
     REQUIRE(lst.Size() == 1UL);
@@ -21,9 +20,8 @@ TEST_CASE("Size() корректно растёт при добавлениях"
     REQUIRE(lst.Size() == 7UL);
 }
 
-TEST_CASE("Clear() очищает список и оставляет его работоспособным", "[list][clear]") {
+TEST_CASE("Clear очищает список и позволяет дальше работать", "[task1][clear]") {
     List lst;
-
     for (int i = 0; i < 5; ++i) lst.PushBack(i);
     REQUIRE(lst.Size() == 5UL);
 
@@ -31,15 +29,13 @@ TEST_CASE("Clear() очищает список и оставляет его ра
     REQUIRE(lst.Empty());
     REQUIRE(lst.Size() == 0UL);
 
+    // Повторный Clear безопасен
     REQUIRE_NOTHROW(lst.Clear());
     REQUIRE(lst.Size() == 0UL);
 
+    // После очистки можно снова вставлять
     lst.PushFront(42);
     lst.PushBack(7);
     REQUIRE_FALSE(lst.Empty());
     REQUIRE(lst.Size() == 2UL);
-
-    lst.Clear();
-    REQUIRE(lst.Empty());
-    REQUIRE(lst.Size() == 0UL);
 }
--- cppl_homeworks_04/task_2/test.cpp
+++ cppl_homeworks_04/task_2/test.cpp
@@ -1,86 +1,57 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_exception.hpp>
-#include "list.cpp"
+#include "list.h"
 
-TEST_CASE("PushBack: элементы уходят в хвост; порядок FIFO через PopFront", "[list][pushback][popfront]") {
+TEST_CASE("PushBack + PopFront дает FIFO-порядок", "[task2][pushback][popfront]") {
     List lst;
-    REQUIRE(lst.Empty());
-    REQUIRE(lst.Size() == 0UL);
-
     lst.PushBack(1);
     lst.PushBack(2);
     lst.PushBack(3);
     REQUIRE(lst.Size() == 3UL);
 
     REQUIRE(lst.PopFront() == 1);
-    REQUIRE(lst.Size() == 2UL);
     REQUIRE(lst.PopFront() == 2);
-    REQUIRE(lst.Size() == 1UL);
     REQUIRE(lst.PopFront() == 3);
     REQUIRE(lst.Empty());
-    REQUIRE(lst.Size() == 0UL);
 }
 
-TEST_CASE("PushFront: элементы уходят в голову; порядок LIFO через PopFront", "[list][pushfront][popfront]") {
+TEST_CASE("PushFront + PopFront дает LIFO-поведение головы", "[task2][pushfront][popfront]") {
     List lst;
-
     lst.PushFront(1);
     lst.PushFront(2);
     lst.PushFront(3);
     REQUIRE(lst.Size() == 3UL);
 
     REQUIRE(lst.PopFront() == 3);
-    REQUIRE(lst.Size() == 2UL);
     REQUIRE(lst.PopFront() == 2);
-    REQUIRE(lst.Size() == 1UL);
     REQUIRE(lst.PopFront() == 1);
     REQUIRE(lst.Empty());
 }
 
-TEST_CASE("PopBack/PopFront на пустом списке бросают runtime_error", "[list][empty][exceptions]") {
+TEST_CASE("PopFront/PopBack на пустом списке бросают runtime_error", "[task2][exceptions]") {
     List lst;
     REQUIRE(lst.Empty());
-
-    REQUIRE_THROWS_WITH(lst.PopFront(), "list is empty");
-    REQUIRE_THROWS_WITH(lst.PopBack(), "list is empty");
-    REQUIRE(lst.Size() == 0UL); // размер не изменился
+    REQUIRE_THROWS_AS(lst.PopFront(), std::runtime_error);
+    REQUIRE_THROWS_AS(lst.PopBack(), std::runtime_error);
+    REQUIRE(lst.Size() == 0UL);
 }
 
-TEST_CASE("Сложный сценарий: чередование операций Push/Pop спереди и сзади", "[list][scenario]") {
-    List lst;
-    // Старт: []
-    lst.PushBack(10);                 // [10]
-    REQUIRE(lst.Size() == 1UL);
-
-    lst.PushFront(5);                 // [5, 10]
-    REQUIRE(lst.Size() == 2UL);
-
-    lst.PushBack(20);                 // [5, 10, 20]
+TEST_CASE("Сложный сценарий чередования операций", "[task2][scenario]") {
+    List lst;                 // []
+    lst.PushBack(10);         // [10]
+    lst.PushFront(5);         // [5, 10]
+    lst.PushBack(20);         // [5, 10, 20]
     REQUIRE(lst.Size() == 3UL);
 
-    REQUIRE(lst.PopFront() == 5);     // [10, 20]
-    REQUIRE(lst.Size() == 2UL);
-
-    lst.PushFront(1);                 // [1, 10, 20]
-    REQUIRE(lst.Size() == 3UL);
-
-    REQUIRE(lst.PopBack() == 20);     // [1, 10]
-    REQUIRE(lst.Size() == 2UL);
-
-    lst.PushBack(30);                 // [1, 10, 30]
-    REQUIRE(lst.Size() == 3UL);
-
-    REQUIRE(lst.PopFront() == 1);     // [10, 30]
-    REQUIRE(lst.Size() == 2UL);
-
-    REQUIRE(lst.PopFront() == 10);    // [30]
-    REQUIRE(lst.Size() == 1UL);
-
-    REQUIRE(lst.PopBack() == 30);     // []
+    REQUIRE(lst.PopFront() == 5); // [10, 20]
+    lst.PushFront(1);             // [1, 10, 20]
+    REQUIRE(lst.PopBack() == 20); // [1, 10]
+    lst.PushBack(30);             // [1, 10, 30]
+    REQUIRE(lst.PopFront() == 1); // [10, 30]
+    REQUIRE(lst.PopFront() == 10);// [30]
+    REQUIRE(lst.PopBack() == 30); // []
     REQUIRE(lst.Empty());
     REQUIRE(lst.Size() == 0UL);
 
-    // убедимся, что на пустом снова бросает
-    REQUIRE_THROWS_WITH(lst.PopFront(), "list is empty");
-    REQUIRE_THROWS_WITH(lst.PopBack(), "list is empty");
+    REQUIRE_THROWS_AS(lst.PopFront(), std::runtime_error);
+    REQUIRE_THROWS_AS(lst.PopBack(), std::runtime_error);
 }
